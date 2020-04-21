29ea98b47909270409240f06251fec0fe7f7b004
ultrafro <hisham.bedri@gmail.com>
2020-04-20 21:13:36 -0400

    router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ThreeCanvas from "./ThreeCanvas.jsx";
+import {
+  BrowserRouter,
+  Router,
+  Switch,
+  Route,
+  Link,
+  hashHistory,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       {/* <header className="App-header"></header> */}
-
-      <ThreeCanvas></ThreeCanvas>
+      <BrowserRouter>
+        <Route path="*" component={ThreeCanvas}></Route>
+      </BrowserRouter>
     </div>
   );
 }
